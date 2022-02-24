ad7d73df93d6d9616ea4404bc890786fca41f5ea
annagustavsson <annalovisaellen@gmail.com>
2022-02-24 17:00:50 +0100

    feat: add new entity toggle menu

--- src/components/ToggleMenu/ToggleMenu.tsx
+++ src/components/ToggleMenu/ToggleMenu.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useHistory, useLocation} from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import ToggleButton from "./ToggleButton/ToggleButton"
 import styles from "./toggleMenu.module.scss"
 
@@ -15,15 +15,22 @@ const ToggleMenu = (props: Props) => {
     let aboutActivated = true
     let projectsActivated = false
 
-
+    // om active är nestad i toggleButton behövs inte detta
     const activeStyles = [styles.active, styles.toggleButton].join(" ")
     const unActiveStyle = styles.toggleButton
 
 
     return (
         <div className={styles.toggleContainer}>
-            <div className={location.pathname === "/" ? activeStyles : unActiveStyle}><ToggleButton  onClick={() => history.push("/")} text={"About"}/></div>
-            <div className={location.pathname === "/projects" ? activeStyles : unActiveStyle}><ToggleButton  onClick={() => history.push("/projects")} text={"Projects"} /></div>
+            <div className={location.pathname === "/" ? activeStyles : unActiveStyle}>
+                <ToggleButton onClick={() => history.push("/")} text={"About"} />
+            </div>
+            <div className={location.pathname === "/projects" ? activeStyles : unActiveStyle}>
+                <ToggleButton onClick={() => history.push("/projects")} text={"Projects"} />
+            </div>
+            <div className={location.pathname === "/experience" ? activeStyles : unActiveStyle}>
+                <ToggleButton onClick={() => history.push("/experience")} text={"Experience"} />
+            </div>
         </div>
     )
 }
